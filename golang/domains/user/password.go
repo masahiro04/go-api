5f6796ce1643502b0856808643d404829ff15030
masahirookubo <stmhamachiii@gmail.com>
2022-02-01 01:26:32 +0900

    userのdomain追加

---
+++ golang/domains/user/password.go
@@ -0,0 +1,32 @@
+package user
+
+import (
+	"clean_architecture/golang/domains/validator"
+)
+
+type Password struct {
+	// [Blogの説明を表現する値オブジェクト]
+	// バリデーションルールは以下
+	// - 空ではないこと
+	// - 100文字以下であること
+	Value string `validate:"required,gte=6" ja:"パスワード"`
+}
+
+func NewPassword(value string) (Password, error) {
+	password := Password{Value: value}
+	err := validator.Validate(password)
+	if err != nil {
+		return password, err
+	}
+	return password, nil
+}
+
+func UpdatePassword(input *string) (*Password, error) {
+	password := Password{Value: *input}
+	err := validator.Validate(&password)
+
+	if err != nil {
+		return &password, err
+	}
+	return &password, nil
+}
